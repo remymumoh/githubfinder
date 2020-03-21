c4c30537e3df92a4190b5401708d09b462e3282b
Raymond M. Ngei <rngei@umsomihvkenya.local>
2020-03-21 19:43:50 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,123 @@
+import React, { Component, Fragment } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./components/layouts/Navbar";
+import Users from "./components/users/Users";
+import User from "./components/users/User";
+import Search from "./components/users/Search";
+import axios from "axios";
+import Alert from "./components/layouts/Alert";
+import About from "./components/pages/About";
+import "./App.css";
+
+class App extends Component {
+  state = {
+    users: [],
+    user:{},
+    repos:[],
+    loading: false,
+    alert: null
+  };
+
+  //  async componentDidMount(){
+
+  //    this.setState({loading: true})
+
+  //     const res = await axios.get(`https://api.github.com/users?client_id=${
+  //           process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${
+  //             process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+  //             this.setState({users: res.data, loading:false});
+
+  //   }
+
+  //search github users
+  searchUsers = async text => {
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  //get single github user
+  getUser = async(username) => {
+
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ user: res.data, loading: false });
+
+  }
+
+  //get single github user
+  getUserRepos = async(username) => {
+
+    this.setState({ loading: true });
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+
+    this.setState({ repos: res.data, loading: false });
+
+  }
+
+
+  //clear users from request
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  //set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert }), 5000);
+  };
+  render() {
+    const { users, user, repos, loading } = this.state;
+
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Switch>
+              <Route
+                exact
+                path='/'
+                render={props => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              />
+              <Route exact path='/about' component={About}/>
+              <Route 
+              exact path='/user/:login' 
+              render={props => (
+                //user component
+                <User 
+                {...props} 
+                getUser={this.getUser} 
+                getUserRepos={this.getUserRepos}
+                user={user}
+                repos={repos}
+                loading={loading}/>
+              )} />
+            </Switch>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
